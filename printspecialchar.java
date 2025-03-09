062f49fae4fda65a9f43f4da62a1f6be4087df03
Vineet Chhikara <vinitchhikara12@gmail.com>
2025-03-09 21:37:34 +0530

    first program

---
+++ printspecialchar.java
@@ -0,0 +1,14 @@
+class printspecialchar{
+    public static void main(String[] args) {
+        // print the information of 5 people
+        // Name Email Employee-code phone
+        System.out.println("Vineet\tvineet@email\temp001\t00001212");
+        System.out.println("Arun\tarun@email\temp002\t32401212");
+        System.out.println("Jatin\tjatin@email\temp003\t34201212");
+        System.out.println("Aniket\taniket@email\temp004\t42301212");
+        System.out.println("Mohit\tmohit@email\temp005\t32901212");
+        System.out.println();
+        // print ""can we print'\' with System.out.println() statement?""
+        System.out.println("\"can we print\'\\\' with System.out.println() statement?\"");
+    }
+}
