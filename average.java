0896ad0794f3e12b878f915dd3c2264d4e6e2125
Vineet Chhikara <vinitchhikara12@gmail.com>
2025-03-10 22:30:00 +0530

    Average synset of months

---
+++ average.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the synsets for the month of April :");
+        int april =sc.nextInt();
+        System.out.print("Enter the synsets for the month of May :");
+        int may =sc.nextInt();
+        System.out.print("Enter the synsets for the month of June :");
+        int june =sc.nextInt();
+        int result = (april+may+june)/3;
+        System.out.print("Average Synset Entered :"+result);
+    }
+}
